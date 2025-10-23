71a53c063a82f6890c7d6eb17519abb837f3574a
jpinglove <jpinglove@163.com>
2025-10-23 13:28:01 +0800

    update

--- models/product.model.js
+++ models/product.model.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const ProductSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
-    description: { type: String, required: true },
+    description: { type: String, default: "" },
     price: { type: Number, required: true },
     category: { type: String, required: true },
 
